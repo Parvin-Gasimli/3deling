a7fdd518c1cd13b9da561fcf4f36015c8497d055
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2022-03-16 16:49:28 +0300

    Some changes

--- assests/js/main.js
+++ assests/js/main.js
@@ -38,87 +38,20 @@ AOS.init({
   throttleDelay: 99,
   debounceDelay: 50
 });
-var elem = document.querySelector('.grid');
-var iso = new Isotope(elem, {
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    name: '.name',
-  }
-});
-
-// element argument can be a selector string
-//   for an individual element
-var iso = new Isotope('.grid', {
-  // options
-});
-
-// filter functions
-var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function (itemElem) {
-    var number = itemElem.querySelector('.number').textContent;
-    return parseInt(number, 10) > 50;
-  },
-  // show if name ends with -ium
-  ium: function (itemElem) {
-    var name = itemElem.querySelector('.name').textContent;
-    return name.match(/ium$/);
-  }
-};
-
-// bind filter button click
-var filtersElem = document.querySelector('.filters-button-group');
-filtersElem.addEventListener('click', function (event) {
-  // only work with buttons
-  if (!matchesSelector(event.target, 'button')) {
-    return;
-  }
-  var filterValue = event.target.getAttribute('data-filter');
-  // use matching filter function
-  filterValue = filterFns[filterValue] || filterValue;
-  iso.arrange({ filter: filterValue });
-});
-
-// change is-checked class on buttons
-var buttonGroups = document.querySelectorAll('.button-group');
-for (var i = 0, len = buttonGroups.length; i < len; i++) {
-  var buttonGroup = buttonGroups[i];
-  radioButtonGroup(buttonGroup);
-}
-
-function radioButtonGroup(buttonGroup) {
-  buttonGroup.addEventListener('click', function (event) {
-    // only work with buttons
-    if (!matchesSelector(event.target, 'button')) {
-      return;
+$(document).ready(function(){
+  $grid = $('.list').isotope({
+    // options
+    itemSelector: '.list__item',
+    layoutMode: 'masonry',
+    masonry: {
+      gutter: 0
     }
-    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-    event.target.classList.add('is-checked');
   });
-}
-
-
-
-
-
-(function ($) {
-  var duration = 2500;  // change this to change rotation time in milliseconds
-  var current = 1;
-  var tricker = $(".tricker");
-  var height = tricker.height();
-  var number = tricker.children().length;
-  var first = tricker.children().first();
-
-  setInterval(function () {
-    var interv = current * -1 * height;
-    first.css("margin-top", interv + "px");
-    if (current == number) {
-      first.css("margin-top", "0px");
-      current = 1;
-    } else {
-      current++;
-    }
-  }, duration);
-})(jQuery);
+  // filter items on button click
+  $('.js-filter').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    $( '.js-filter button' ).removeClass( 'is-active' );
+    $( this ).addClass( 'is-active' );
+  });
+});
